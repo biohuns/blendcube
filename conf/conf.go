d035a8c8584983ae02e166a00385adc9a0f08314
Shinobu Nishioka <biohuns@gmail.com>
2019-11-09 00:54:52 +0900

    add: handler

---
+++ conf/conf.go
@@ -0,0 +1,90 @@
+package conf
+
+import (
+	"io"
+	"log"
+	"os"
+	"strconv"
+	"time"
+
+	"github.com/go-chi/chi/middleware"
+	"github.com/spf13/viper"
+)
+
+type (
+	Config struct {
+		Server struct {
+			Port    int           `json:"port"`
+			Timeout time.Duration `json:"timeout"`
+		} `json:"server"`
+		Log struct {
+			StdOutput  bool   `json:"std_output"`
+			FileOutput bool   `json:"file_output"`
+			FilePath   string `json:"file_path"`
+			NoColor    bool   `json:"no_color"`
+		} `json:"log"`
+	}
+)
+
+var (
+	Shared *Config
+)
+
+// GetPort
+func (c *Config) GetPort() string {
+	return ":" + strconv.Itoa(c.Server.Port)
+}
+
+// Configure
+func Configure() error {
+	viper.SetConfigName("config")
+	viper.SetConfigType("toml")
+	viper.SetConfigFile("./config.toml")
+	viper.AutomaticEnv()
+
+	if err := viper.ReadInConfig(); err != nil {
+		return err
+	}
+
+	if err := viper.Unmarshal(&Shared); err != nil {
+		return err
+	}
+
+	if err := configureLogger(); err != nil {
+		return err
+	}
+
+	return nil
+}
+
+func configureLogger() error {
+	var writer []io.Writer
+
+	if Shared.Log.StdOutput {
+		writer = append(writer, os.Stdout)
+	}
+	if Shared.Log.FileOutput {
+		Shared.Log.NoColor = true
+		logFile, err := os.OpenFile(
+			Shared.Log.FilePath,
+			os.O_APPEND|os.O_CREATE|os.O_WRONLY,
+			0644,
+		)
+		if err != nil {
+			return err
+		}
+		writer = append(writer, logFile)
+	}
+
+	middleware.DefaultLogger = middleware.RequestLogger(
+		&middleware.DefaultLogFormatter{
+			Logger: log.New(
+				io.MultiWriter(writer...),
+				"",
+				log.LstdFlags),
+			NoColor: Shared.Log.NoColor,
+		},
+	)
+
+	return nil
+}
--- go.mod
+++ go.mod
@@ -1,3 +1,18 @@
 module blendcube
 
 go 1.13
+
+require (
+	github.com/go-chi/chi v4.0.2+incompatible
+	github.com/go-playground/locales v0.13.0 // indirect
+	github.com/go-playground/validator v9.30.0+incompatible // indirect
+	github.com/leodido/go-urn v1.2.0 // indirect
+	github.com/pelletier/go-toml v1.6.0 // indirect
+	github.com/qmuntal/gltf v0.9.1 // indirect
+	github.com/spf13/afero v1.2.2 // indirect
+	github.com/spf13/jwalterweatherman v1.1.0 // indirect
+	github.com/spf13/pflag v1.0.5 // indirect
+	github.com/spf13/viper v1.5.0
+	golang.org/x/sys v0.0.0-20191105231009-c1f44814a5cd // indirect
+	gopkg.in/yaml.v2 v2.2.5 // indirect
+)
